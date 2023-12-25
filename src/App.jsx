d56005594b4f4fc6bcbbd27932d0b3577d3e7817
Lahiru Wimalarathna <136983403+lahiruC22@users.noreply.github.com>
2023-12-25 10:09:54 +0000

    update the site content

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Lahiru Wimalarathna",
+  title: "Software Engineer",
+  email: "lahirucw1@gmail.com",
+  gitHub: "https://github.com/lahiruC22",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/lahiru-wimalarathna-72bb46285/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: ",
 };
 
 const primaryColor = "#4E567E";
